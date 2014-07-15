7bcd28cc44285ac02232b0ee4b2c4f0d1fee463c
Kim Dong Ryul <kdr1080@nate.com>
2014-07-15 20:27:49 +0900

    homepage making files

---
+++ myclass/homepage.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+		
+		$('#no1').css('display','none').fadeIn(3000).fadeOut(3000).delay(3000).delay(3000); 
+		$('#no2').css('display','none').delay(3000).fadeIn(3000).fadeOut(3000).delay(3000);
+		$('#no3').css('display','none').delay(3000).delay(3000).fadeIn(3000).fadeOut(3000);
+		$('#no4').fadeOut(3000).delay(3000).delay(3000).fadeIn(3000);
+
+});
+
