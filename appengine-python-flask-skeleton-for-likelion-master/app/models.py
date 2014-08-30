c42c7b4ef5681ee94c7826335c89fc5e21cb263e
Kim Dong Ryul <kdr1080@nate.com>
2014-08-30 18:27:55 +0900

    dc

--- appengine-python-flask-skeleton-for-likelion-master/app/models.py
+++ appengine-python-flask-skeleton-for-likelion-master/app/models.py
@@ -4,15 +4,16 @@ class Team(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	teamname = db.Column(db.String(255))
 	location_first = db.Column(db.String(255))
-	location_second= db.Column(db.String(255))
 	university= db.Column(db.String(255))
 	Introduce = db.Column(db.Text())
 	
 class Location_first(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	location_first = db.Column(db.String(255))
+	upper_id=db.Column(db.Integer(), default=0)
 
-class Location_second(db.Model):
+
+class University(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	location_second = db.Column(db.String(255))
-	
+	university = db.Column(db.String(255))
+	location_id=upper_id=db.Column(db.Integer(), default=0)
--- appengine-python-flask-skeleton-for-likelion-master/app/settings.py
+++ appengine-python-flask-skeleton-for-likelion-master/app/settings.py
@@ -1,9 +1,9 @@
+
 class Config(object):
     SECRET_KEY = 'sldkjflksjdlfk'
     FACEBOOK_APP_ID = '754223001285432'
     FACEBOOK_APP_SECRET = '3efb03300bcfaf56048ac87c2f758ac5'
     debug = False
-
 
 class Production(Config):
     debug = True
